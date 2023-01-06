73508e46f79beff3c1d4484c753ac7c60bd610d8
conwys <drkheartt@gmail.com>
2023-01-06 16:24:08 +0000

    learn page started

--- locodemotive/src/pages/Learn.js
+++ locodemotive/src/pages/Learn.js
@@ -1,7 +1,39 @@
+import './Learn.css'
+
 export default function Learn() {
     return (
-        <>
-            <h1>Learn</h1>
-        </>
+        <div className="learn-content">
+            <div className="topic-list-container">
+                <h1>Topic</h1>
+                <div className="topics">
+                    <h2>General</h2>
+                    <h2>Digital</h2>
+                    <h2>Placeholder</h2>
+                    <h2>Placeholder</h2>
+                    <h2>Placeholder</h2>
+                    <h2>Placeholder</h2>
+                    <h2>Placeholder</h2>
+                </div>
+            </div>
+            <div className="explore-container">
+                <div className="explore-header">
+                    <div className="explore-header-top">
+                        <h1>Explore</h1>
+                        <a href="./add">Add Resource</a>
+                    </div>
+                    <div className="explore-header-selectors">
+                        <select name="Type">
+                            <option>Placeholder</option>
+                            <option>Placeholder</option>
+                        </select>
+                        <div className="page-buttons">
+                            <button>Back</button>
+                            <button>Forward</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
