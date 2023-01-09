097ee21fb4ff6ad015291faba664d62746139ebd
conwys <drkheartt@gmail.com>
2023-01-09 12:25:15 +0000

    new post page styling

--- locodemotive/src/components/NewPost/NewPost.js
+++ locodemotive/src/components/NewPost/NewPost.js
@@ -1,15 +1,19 @@
+import './NewPost.css'
+
 export default function NewPost() {
     return (
         <div className="new-post">
             <h1>New Post</h1>
-            <input type="text" placeholder="Title"></input>
-            <input type="text" placeholder="Post Body"></input>
-            <select placeholder="Topic">
-                <option>Topic 1</option>
-                <option>Topic 2</option>
-                <option>Topic 3</option>
-            </select>
-            <input type="text" placeholder="Notify a team member"></input>
+            <div className='new-post-submission'>
+                <input type="text" placeholder="Title"></input>
+                <textarea type="text" placeholder="Post Body" wordWrap="false"></textarea>
+                <select placeholder="Topic">
+                    <option>Topic 1</option>
+                    <option>Topic 2</option>
+                    <option>Topic 3</option>
+                </select>
+                <input type="text" placeholder="Notify a team member"></input>
+            </div>
             <button type="submit">Submit</button>
         </div>
     )
