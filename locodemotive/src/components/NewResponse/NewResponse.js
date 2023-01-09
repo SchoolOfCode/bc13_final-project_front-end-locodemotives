385dd400deb813f0fc57e36afcac404b67aaa4e7
conwys <drkheartt@gmail.com>
2023-01-09 12:29:34 +0000

    new response styling

--- locodemotive/src/components/NewResponse/NewResponse.js
+++ locodemotive/src/components/NewResponse/NewResponse.js
@@ -1,8 +1,12 @@
+import './NewResponse.css'
+
 export default function NewResponse() {
     return (
         <div className="new-response">
             <h1>New Response</h1>
-            <input type="text" placeholder="Response Body"></input>
+            <div className='new-response-submission'>
+                <textarea type="text" placeholder="Response Body"></textarea>
+            </div>
             <button type="submit">Submit</button>
         </div>
     )
