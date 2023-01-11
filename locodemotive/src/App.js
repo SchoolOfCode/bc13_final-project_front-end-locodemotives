d51ce0317bdc3bd2ea4a493befd659c904a15b97
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-11 11:36:48 +0000

    user image on navbar

--- locodemotive/src/App.js
+++ locodemotive/src/App.js
@@ -1,45 +1,52 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home.js'
-import Learn from './pages/Learn.js'
-import Discuss from './pages/Discuss.js'
-import Settings from './pages/Settings.js'
-import Login from './pages/Login';
-import CreateResponse from './pages/CreateResponse';
-import CreatePost from './pages/CreatePost';
-import CreateResource from './pages/CreateResource';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import PrivateRoute from './PrivateRoute';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home.js";
+import Learn from "./pages/Learn.js";
+import Discuss from "./pages/Discuss.js";
+import Settings from "./pages/Settings.js";
+import Login from "./pages/Login";
+import CreateResponse from "./pages/CreateResponse";
+import CreatePost from "./pages/CreatePost";
+import CreateResource from "./pages/CreateResource";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import PrivateRoute from "./PrivateRoute";
 // import env from 'react-dotenv';
 
 function App() {
-
   const navigate = useNavigate();
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState({});
 
   async function getUser(email, password) {
-    let userFetch = await fetch(`${process.env.REACT_APP_URL}/user/?email=${email}`);
+    let userFetch = await fetch(
+      `${process.env.REACT_APP_URL}/user/?email=${email}`
+    );
     let userData = await userFetch.json();
-    if (email === userData?.payload?.email && userData.payload.email.length > 0) {
-      if (password === userData.payload.password && userData.payload.password.length > 0) {
+    if (
+      email === userData?.payload?.email &&
+      userData.payload.email.length > 0
+    ) {
+      if (
+        password === userData.payload.password &&
+        userData.payload.password.length > 0
+      ) {
         login();
-        setUser(userData.payload); 
+        setUser(userData.payload);
       } else {
         setUser({});
-        return "Incorrect Password!"
+        return "Incorrect Password!";
       }
     } else {
       setUser({});
-      return "Incorrect Email!"
+      return "Incorrect Email!";
     }
   }
 
   const login = () => {
     setIsAuthenticated(true);
-    navigate('/home')
+    navigate("/home");
   };
 
   const logout = () => {
@@ -47,61 +54,89 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <Navbar isAuthenticated={isAuthenticated} login={getUser} logout={logout}/>
-        <Routes>
-          <Route path='/' element={<Navigate to='/login'/>}/>
-          <Route path='/login' element={<Login login={getUser} logout={logout}/>} />
-          <Route path='/home' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<Home/>} 
+    <div className="App">
+      <Navbar
+        isAuthenticated={isAuthenticated}
+        logout={logout}
+        userData={user}
+      />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={<Login login={getUser} logout={logout} />}
+        />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<Home />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/learn' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<Learn/>} 
+          }
+        />
+        <Route
+          path="/learn"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<Learn />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/discuss' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<Discuss/>} 
+          }
+        />
+        <Route
+          path="/discuss"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<Discuss />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/settings' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<Settings/>} 
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<Settings />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/new_response' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<CreateResponse/>} 
+          }
+        />
+        <Route
+          path="/new_response"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<CreateResponse />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/new_post' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<CreatePost/>} 
+          }
+        />
+        <Route
+          path="/new_post"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<CreatePost />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-          <Route path='/new_resource' element={
-            <PrivateRoute 
-              redirectTo="/login" 
-              component={<CreateResource/>} 
+          }
+        />
+        <Route
+          path="/new_resource"
+          element={
+            <PrivateRoute
+              redirectTo="/login"
+              component={<CreateResource />}
               isAuthenticated={isAuthenticated}
             />
-          }/>
-        </Routes>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- locodemotive/src/components/Navbar/Navbar.js
+++ locodemotive/src/components/Navbar/Navbar.js
@@ -1,55 +1,55 @@
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
-import './Navbar.css'
-import Logo from './Logo.png';
-import Bell from './Bell.svg';
-import ProfilePlaceholder from './ProfilePlaceholder.png'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "./Logo.png";
+import Bell from "./Bell.svg";
+// import ProfilePlaceholder from "./ProfilePlaceholder.png";
 
-export default function Navbar({ isAuthenticated, logout}) {
-    return (
-        <nav className="nav">
-            <Link to="/home" className="site-title"><img src={Logo} alt="Locodemotive Logo"></img></Link>
-            <ul>
-            {isAuthenticated ? (
-                <button onClick={logout}>Log-out</button>
-            ) : (
-                <></>
-            )}
-            {isAuthenticated ? (
-                <CustomLink to="/home">Home</CustomLink>
-            ) : (
-                <></>
-            )}
-            {isAuthenticated ? (
-                <CustomLink to="/learn">Learn</CustomLink>
-            ) : (
-                <></>
-            )}
-            {isAuthenticated ? (
-                <CustomLink to="/discuss">Discuss</CustomLink>
-            ) : (
-                <></>
-            )}
-            {isAuthenticated ? (
-                <li><button id='bell'><img src={Bell} alt="Notification Bell"></img></button></li>
-            ) : (
-                <></>
-            )}
-            {isAuthenticated ? (
-                <CustomLink to="/settings"><span><img id="pfp" src={ProfilePlaceholder} alt="ProfileImage"></img></span></CustomLink>
-            ) : (
-                <></>
-            )}
-            </ul>
-        </nav>
-    )
+export default function Navbar({ isAuthenticated, logout, userData }) {
+  return (
+    <nav className="nav">
+      <Link to="/home" className="site-title">
+        <img src={Logo} alt="Locodemotive Logo"></img>
+      </Link>
+      <ul>
+        {isAuthenticated ? <button onClick={logout}>Log-out</button> : <></>}
+        {isAuthenticated ? <CustomLink to="/home">Home</CustomLink> : <></>}
+        {isAuthenticated ? <CustomLink to="/learn">Learn</CustomLink> : <></>}
+        {isAuthenticated ? (
+          <CustomLink to="/discuss">Discuss</CustomLink>
+        ) : (
+          <></>
+        )}
+        {isAuthenticated ? (
+          <li>
+            <button id="bell">
+              <img src={Bell} alt="Notification Bell"></img>
+            </button>
+          </li>
+        ) : (
+          <></>
+        )}
+        {isAuthenticated ? (
+          <CustomLink to="/settings">
+            <span>
+              <img id="pfp" src={userData.image_url} alt="ProfileImage"></img>
+            </span>
+          </CustomLink>
+        ) : (
+          <></>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-    return (
-        <li className={isActive ? "active" : ""}>
-            <Link to={to} {...props}>{children}</Link>
-        </li>
-    )
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
