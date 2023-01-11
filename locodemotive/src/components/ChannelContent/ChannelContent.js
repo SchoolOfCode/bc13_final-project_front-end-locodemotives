3d7b35381fa8657ed31aab3fd8f2d5246c59d967
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-11 12:29:06 +0000

    added links to channels on home page

--- locodemotive/src/components/ChannelContent/ChannelContent.js
+++ locodemotive/src/components/ChannelContent/ChannelContent.js
@@ -1,23 +1,29 @@
-import Pluralsight from './Images/Pluralsight.png';
-import Sharepoint from './Images/Sharepoint.png';
-import Resonate from './Images/Resonate.png';
-import './ChannelContent.css'
+import Pluralsight from "./Images/Pluralsight.png";
+import Sharepoint from "./Images/Sharepoint.png";
+import Resonate from "./Images/Resonate.png";
+import "./ChannelContent.css";
 
 export default function ChannelContent() {
-    return (
-        <div className="channel-container">
-            <div className='channel'>
-                <img src={Pluralsight} alt="Pluralsight"></img>
-                <h2>Pluralsight</h2>
-            </div>
-            <div className='channel'>
-                <img src={Sharepoint} alt="Sharepoint"></img>
-                <h2>Sharepoint</h2>
-            </div>
-            <div className='channel'>
-                <img src={Resonate} alt="Resonate"></img>
-                <h2>Resonate</h2>
-            </div>
+  return (
+    <div className="channel-container">
+      <a href="https://app.pluralsight.com/id">
+        <div id="pluralsight-channel" className="channel">
+          <img src={Pluralsight} alt="Pluralsight"></img>
+          <h2>Pluralsight</h2>
         </div>
-    )
+      </a>
+      <a href="https://www.microsoft.com/en-gb/microsoft-365/sharepoint/collaboration">
+        <div id="sharepoint-channel" className="channel">
+          <img src={Sharepoint} alt="Sharepoint"></img>
+          <h2>Sharepoint</h2>
+        </div>
+      </a>
+      <a href="https://www.resonate.tech/ui/content/content.aspx?id=7">
+        <div id="resonate-channel" className="channel">
+          <img src={Resonate} alt="Resonate"></img>
+          <h2>Resonate</h2>
+        </div>
+      </a>
+    </div>
+  );
 }
