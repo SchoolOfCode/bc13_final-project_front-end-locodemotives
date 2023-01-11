1061153332600b9274154f8ec5a1626b5bd653c0
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-11 16:58:12 +0000

    started css

--- locodemotive/src/pages/Discuss.js
+++ locodemotive/src/pages/Discuss.js
@@ -20,12 +20,12 @@ export default function Discuss({ userData }) {
     <div className="discuss-content">
       <div className="navigation-bar">
         <input type="text" placeholder="Search"></input>
-        <button type="submit">Submit</button>
         <select>
           <option>Topic 1</option>
           <option>Topic 2</option>
           <option>Topic 3</option>
         </select>
+        <button type="submit">Submit</button>
         <CustomLink to="/new_post">New Post</CustomLink>
       </div>
       <div className="post-container">
