9f36ece51456f02e97596d033bdb9b6105434bef
romanhadjisergis <rhadjisergis@hotmail.com>
2023-01-11 16:58:31 +0000

    nearly done new resources

--- locodemotive/src/components/NewResource/NewResource.js
+++ locodemotive/src/components/NewResource/NewResource.js
@@ -1,25 +1,90 @@
-import './NewResource.css'
+import "./NewResource.css";
+import { useState } from "react";
 
 export default function NewResource() {
-    return (
+  const [newResourceInfo, setResourceInfo] = useState({
+    title: "",
+    description: "",
+    link: "",
+    topic: "",
+    type: "",
+  });
+
+  const handleClickSubmit = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_URL}/resources`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newResourceInfo),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
     <div className="new-resource">
-        <h1>New Resource</h1>
-        <div className='new-resource-submission'>
-            <input type="text" placeholder="Title"></input>
-            <textarea type="text" placeholder="Resource Description"></textarea>
-            <input type="text" placeholder="Resource Link"></input>
-            <select placeholder="Topic">
-                <option>Topic 1</option>
-                <option>Topic 2</option>
-                <option>Topic 3</option>
-            </select>
-            <select placeholder="Type">
-                <option>Type 1</option>
-                <option>Type 2</option>
-                <option>Type 3</option>
-            </select>
-            <button type="submit">Submit</button>
-        </div>
+      <h1>New Resource</h1>
+      <div className="new-resource-submission">
+        <input
+          type="text"
+          placeholder="Title"
+          required
+          onChange={(e) => {
+            newResourceInfo.title = e.target.value;
+            setResourceInfo(newResourceInfo);
+          }}
+        ></input>
+        <textarea
+          type="text"
+          placeholder="Resource Description"
+          required
+          onChange={(e) => {
+            newResourceInfo.description = e.target.value;
+            setResourceInfo(newResourceInfo);
+          }}
+        ></textarea>
+        <input
+          type="text"
+          placeholder="Resource Link"
+          required
+          onChange={(e) => {
+            newResourceInfo.link = e.target.value;
+            setResourceInfo(newResourceInfo);
+          }}
+        ></input>
+        <select
+          placeholder="Topic"
+          name="Topic"
+          required
+          onChange={(e) => {
+            newResourceInfo.topic = e.target.value;
+            setResourceInfo(newResourceInfo);
+          }}
+        >
+          <option>DevOps</option>
+          <option>Support Services</option>
+          <option>Digital Delivery</option>
+          <option>Business Analysis</option>
+        </select>
+        <select
+          placeholder="Type"
+          required
+          onChange={(e) => {
+            newResourceInfo.type = e.target.value;
+            setResourceInfo(newResourceInfo);
+          }}
+        >
+          <option>Type 1</option>
+          <option>Type 2</option>
+          <option>Type 3</option>
+        </select>
+        <button onClick={handleClickSubmit}>Submit</button>
+      </div>
     </div>
-    )
+  );
 }
--- locodemotive/src/components/Post/Post.js
+++ locodemotive/src/components/Post/Post.js
@@ -1,23 +1,23 @@
-import Response from "../Response/Response"
-import './Post.css'
+import Response from "../Response/Response";
+import "./Post.css";
 
 export default function Post() {
-    return (
-        <div className="post-container">
-            <div className="post">
-                <div className="post-info">
-                    <h1>Title</h1>
-                    <div className="topic-container">
-                        <h3>Topic</h3>
-                    </div>
-                    <h2>Author</h2>
-                    <h2>Date</h2>
-                </div>
-                <div className="post-body">
-                    <p>Post body</p>
-                </div>
-            </div>
-            <Response/>
+  return (
+    <div className="post-container">
+      <div className="post">
+        <div className="post-info">
+          <h1>Title</h1>
+          <div className="topic-container">
+            <h3>Topic</h3>
+          </div>
+          <h2>Author</h2>
+          <h2>Date</h2>
         </div>
-    )
+        <div className="post-body">
+          <p>Post body</p>
+        </div>
+      </div>
+      <Response />
+    </div>
+  );
 }
