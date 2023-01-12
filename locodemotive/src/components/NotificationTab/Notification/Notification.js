441b6dacb51b601123063698a41afddda566c946
conwys <drkheartt@gmail.com>
2023-01-12 13:40:38 +0000

    notification started

---
+++ locodemotive/src/components/NotificationTab/Notification/Notification.js
@@ -0,0 +1,10 @@
+import '../NotificationTab.css'
+
+export default function Notification() {
+    return (
+        <div className='notification'>
+            <img src="" alt="notif-pfp"></img>
+            <p>User replied to your post!</p>
+        </div>
+    )
+}
---
+++ locodemotive/src/components/NotificationTab/NotificationTab.js
@@ -0,0 +1,9 @@
+import Notification from "./Notification/Notification"
+
+export default function NotificationTab() {
+    return (
+        <div className="notification-tab">
+            <h1>Notifications</h1>
+        </div>
+    )
+}
