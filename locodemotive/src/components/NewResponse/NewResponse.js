43a8a95f01c52ba2b836ea44163e0a94c5e1910e
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-12 12:03:05 +0000

    creating a new response with fetch request

--- locodemotive/src/components/NewResponse/NewResponse.js
+++ locodemotive/src/components/NewResponse/NewResponse.js
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import "./NewResponse.css";
 
 export default function NewResponse({ author_id, post_id }) {
+  const [body, setBody] = useState("");
+
+  async function submitNewResponse() {
+    let newResponseJSON = await fetch(
+      `${process.env.REACT_APP_URL}/posts/reply`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          post: post_id,
+          author: author_id,
+          body: body,
+          date_created: new Date(),
+        }),
+      }
+    );
+    await newResponseJSON.json();
+  }
+
   return (
     <div className="new-response">
       <h1>New Response</h1>
       <div className="new-response-submission">
-        <textarea type="text" placeholder="Response Body"></textarea>
+        <textarea
+          type="text"
+          placeholder="Response Body"
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" onClick={submitNewResponse}>
+        Submit
+      </button>
     </div>
   );
 }
