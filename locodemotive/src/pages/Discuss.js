8ee5959567a88ed8f0dc1272a23db5d36b53235c
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-12 09:51:06 +0000

    searching for posts

--- locodemotive/src/pages/Discuss.js
+++ locodemotive/src/pages/Discuss.js
@@ -5,11 +5,35 @@ import { useEffect, useState } from "react";
 
 export default function Discuss({ userData }) {
   let [posts, setPosts] = useState([]);
+  let [searchTitle, setSearchTitle] = useState("null");
+  let [searchTopic, setSearchTopic] = useState("null");
+  let [topics, setTopics] = useState([]);
 
   async function getAllPosts() {
     let allPostsJSON = await fetch(`${process.env.REACT_APP_URL}/posts`);
     let allPosts = await allPostsJSON.json();
     setPosts(allPosts.payload);
+    await findTopics();
+  }
+
+  async function findTopics() {
+    for (let i = 0; i < posts.length; i++) {
+      setTopics([...topics, posts[i].topic]);
+    }
+  }
+
+  async function searchForPosts() {
+    if (searchTitle === "" || searchTitle === " ") {
+      setSearchTitle("null");
+    }
+    if (searchTitle === "null" && searchTopic === "null") {
+      return getAllPosts();
+    }
+    let searchPostsJSON = await fetch(
+      `${process.env.REACT_APP_URL}/posts/search/?title=${searchTitle}&topic=${searchTopic}`
+    );
+    let searchPosts = await searchPostsJSON.json();
+    setPosts(searchPosts.payload);
   }
 
   useEffect(() => {
@@ -19,18 +43,37 @@ export default function Discuss({ userData }) {
   return (
     <div className="discuss-content">
       <div className="navigation-bar">
-        <input type="text" placeholder="Search"></input>
-        <select>
-          <option>Topic 1</option>
-          <option>Topic 2</option>
-          <option>Topic 3</option>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => {
+            setSearchTitle(e.target.value);
+          }}
+        ></input>
+        <select
+          onChange={(e) => {
+            setSearchTopic(e.target.value);
+          }}
+        >
+          <option value={"null"}>Select Topic</option>
+          {/* <option value={"PostgreSQL"}>PostgreSQL</option>
+          <option value={"CSS"}>CSS</option> */}
+          {topics.map((topic, index) => {
+            return (
+              <option value={topic} key={index}>
+                {topic}
+              </option>
+            );
+          })}
         </select>
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={searchForPosts}>
+          Submit
+        </button>
         <CustomLink to="/new_post">New Post</CustomLink>
       </div>
       <div className="post-container">
-        {posts.map((postData) => {
-          return <Post postData={postData} userData={userData} />;
+        {posts.map((postData, index) => {
+          return <Post key={index} postData={postData} userData={userData} />;
         })}
       </div>
     </div>
