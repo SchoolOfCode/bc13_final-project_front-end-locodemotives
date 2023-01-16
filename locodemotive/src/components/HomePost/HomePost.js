68958560c646dc7dbefe281bac3bf75d6524e5e0
romanhadjisergis <rhadjisergis@hotmail.com>
2023-01-16 10:43:37 +0000

    started css for home posts

--- locodemotive/src/components/HomePost/HomePost.js
+++ locodemotive/src/components/HomePost/HomePost.js
@@ -1,11 +1,14 @@
-// import { useState } from "react";
+import "./HomePost.css";
 
 const HomePost = (props) => {
   return (
-    <div>
-      <h3>{props.topic}</h3>
+    <div className="home-post-container">
+      <div className="date-topic-container">
+        <h3>{props.topic}</h3>
+        <h3>{props.date}</h3>
+      </div>
       <h2>{props.title}</h2>
-      <h3>{props.date}</h3>
+      <h1>&gt;</h1>
     </div>
   );
 };
