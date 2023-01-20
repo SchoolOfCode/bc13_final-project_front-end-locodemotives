7d8d177bdb791e56863ad9995e0f828af3eac78a
conwys <drkheartt@gmail.com>
2023-01-20 16:52:56 +0000

    new font for resource description

--- locodemotive/src/components/Resource/Resource.js
+++ locodemotive/src/components/Resource/Resource.js
@@ -2,21 +2,22 @@ import "./Resource.css";
 
 export default function Resource({ resourceData }) {
   return (
-    <div className="learnTopic-topic-container">
-      <div className="topic-body">
-        <div className="topic-body-info">
-          <h1>{resourceData.title}</h1>
-          <div className="topic-type">
-            <h1>{resourceData.type}</h1>
+    <div className="resource-container">
+      <a href={resourceData.link} target="_blank" rel="noreferrer">
+      <div className="learnTopic-topic-container">
+        <div className="topic-body">
+          <div className="topic-body-info">
+            <h1>{resourceData.title}</h1>
+            <div className="topic-type">
+              <h1>{resourceData.type}</h1>
+            </div>
+            <div className="topic-description">
+              <p>{resourceData.description}</p>
+            </div>
           </div>
-          <div className="topic-description">
-            <p>{resourceData.description}</p>
-          </div>
-          <a href={resourceData.link} target="_blank" rel="noreferrer">
-            <button>Access &gt;</button>
-          </a>
         </div>
       </div>
+      </a>
     </div>
   );
 }
